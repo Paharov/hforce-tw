1430ffda671f513555efd9262a5c3d3b7005d281
Marton Bod <marton.bod@gmail.com>
2018-10-20 21:40:00 +0200

    styling navbar

--- tw-visual/src/NavBar.jsx
+++ tw-visual/src/NavBar.jsx
@@ -6,22 +6,33 @@ import logo from './resources/Transferwise_logo.png'
 import { withStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 
-const styles = theme => ({
+const styles = {
     logo: {
       marginLeft: -12,
       marginRight: 20,
+      marginBottom: 12
+    },
+    card: {
+        size: "20%",
+        marginLeft: "90%"
+    },
+    title: {
+        marginLeft: "auto", 
+        fontFamily: "Ubuntu", 
+        marginRight: "40%", 
+        fontSize: "1.5rem",
+        letterSpacing: "0.05em"
     }
-});
+};
 
 function NavBar(props) {
     const { classes } = props;
     return(
         <AppBar position="static">
             <Toolbar>
-                {<img width="20%" height="20%" src={logo} style={{marginLeft: -12, marginRight: 20}}/>}
-                <Typography variant="title" color="inherit" align="center">
-                    Live transfers visualization
-                </Typography>
+                <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"/>
+                {<img width="13%" height="13%" src={logo} style={styles.logo}/>}
+                <div style={styles.title}>Live transfer visualization</div>
             </Toolbar>
         </AppBar>
     )
