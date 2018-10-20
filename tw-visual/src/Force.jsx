55b68600f19b49ff0ae4e9a4157200c9d8ad9415
Andras Szanto <paharov@gmail.com>
2018-10-21 00:36:28 +0200

    Add coin symbols instead of circles

--- tw-visual/src/Force.jsx
+++ tw-visual/src/Force.jsx
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 import { getFoci, getCurrencyMap } from './helper/foci.js';
 import { getRatesMap, calculateCircleSize } from './helper/converter.js';
-import logo from './resources/transferwise_small_logo.png' 
+import logo from './resources/transferwise_small_logo.png';
+import USD from './images/usd.jpg';
+import CNY from './images/cny.jpg';
+import EUR from './images/eur.jpg';
+import UAH from './images/uah.svg';
+import PHP from './images/php.gif';
+import BRL from './images/brl.png';
+import RUB from './images/rub.svg';
+import IDR from './images/idr.jpg';
+import PLN from './images/pln.jpg';
+import SEK from './images/sek.svg';
 
 class Force extends Component {
     constructor(props) {
@@ -20,7 +30,8 @@ class Force extends Component {
             rates: null,
             height: props.height,
             width: props.width,
-            currencyMap: getCurrencyMap(props.currencies, props.width / 2, (props.height / 2) + props.height * 0.2, Math.min(props.width, props.height) / 1.75)
+            currencyMap: getCurrencyMap(props.currencies, props.width / 2, (props.height / 2) + props.height * 0.2, Math.min(props.width, props.height) / 2.2),
+            images: { "BRL": BRL, "CNY": CNY, "EUR": EUR, "IDR": IDR, "PHP": PHP, "PLN": PLN, "RUB": RUB, "SEK": SEK, "UAH": UAH, "USD": USD }
         }
         this.createBalls = this.createBalls.bind(this);
         this.newBall = this.newBall.bind(this);
@@ -33,17 +44,29 @@ class Force extends Component {
             height = this.state.height;
 
         var nodes = [],
-            foci = getFoci(this.state.currencies.length, width / 2, (height / 2) + height * 0.2, Math.min(width, height) / 1.75).foci;
+            foci = getFoci(this.state.currencies.length, width / 2, (height / 2) + height * 0.2, Math.min(width, height) / 2.2).foci;
 
         var svg = d3.select(this.nodeRef.current.nodeName).append("svg")
             .attr("id", "mainSvg")
             .attr("width", width + width * 0.4)
             .attr("height", height + height * 0.4);
 
+        this.state.currencies.forEach(
+            currency => {
+                svg.append("svg:filter")
+                .attr("id", `${currency}_sign`)
+                .attr("x", "0%")
+                .attr("y", "0%")
+                .attr("width", "100%")
+                .attr("height", "100%")
+                .append("svg:feImage")
+                .attr("xlink:href", this.state.images[currency]);
+        })
+
         var force = d3.layout.force()
             .nodes(nodes)
             .links([])
-            .gravity(0.005)
+            .gravity(0.001)
             .size([width, height])
             .on("tick", this.doTick);
 
@@ -76,7 +99,7 @@ class Force extends Component {
     }
 
     doTick = (e) => {
-        var k = 0.01;
+        var k = 0.005;
 
         // Push nodes toward their designated focus.
         let node = this.state.currNode;
@@ -129,11 +152,13 @@ class Force extends Component {
             .attr("y", srcCoords.y )
             .attr("r", calculateCircleSize(this.state.current.source_amount, 
                                         this.state.rates[this.state.current.src_currency]))
+            .attr("filter", `url(#${this.state.current.src_currency}_sign)`)
             .style("fill", srcCoords.color)
             .style("visibility", "hidden")
             .append("title")
             .text(this.state.current.src_currency + " => " + this.state.current.tgt_currency)
             .call(this.state.force.drag());
+
         this.scheduleTargetChange(circleSelection[0], circleSelection[0].length);
         this.scheduleBallExecution(circleSelection);
         this.setState({
