e40e784026199d59bbebae1c59c4bbdbd56a8847
levi <lakatos.leventea@gmail.com>
2018-10-20 09:51:49 +0200

    Added senders and recipients

--- tw-visual/src/TestStream.jsx
+++ tw-visual/src/TestStream.jsx
@@ -1,10 +1,36 @@
 import testTransfers from './sample_data/transfers.json'
+import recipients from './sample_data/recipient.json'
+import senders from './sample_data/profile-service.json'
 
 export default function* testStream() {
+    const recipientsById = getRecipientsById();
+    const sendersById = getSendersById();
     while (true) {
         for (var element in testTransfers) {
-            console.log(testTransfers[element]);
-            yield testTransfers[element];
+            var transfer = testTransfers[element];
+            var result = {
+                ...transfer, 
+                recipient: recipientsById[transfer.recipient_id],
+                sender: sendersById[transfer.id]
+            } 
+            yield result;
         }
     }
 }
+
+function getRecipientsById() {
+    const result = {};
+    recipients.forEach(recipient => {
+        result[recipient.id] = recipient;
+    });
+    return result;
+}
+
+function getSendersById(){
+    let result= {};
+    senders.forEach(sender => {
+        result[sender.id] = sender;
+    });
+    return result;
+}
+
--- tw-visual/src/Visual.jsx
+++ tw-visual/src/Visual.jsx
@@ -2,26 +2,32 @@ import testStream from './TestStream'
 import React, { Component } from 'react';
 
 class Visual extends Component {
+
+
     constructor(props) {
         super(props);
         this.state = {
             streamGen: testStream(),
-            current: null
+            current: null,
+            interval: 1000
         }
     }
 
+    getInterval = () => {
+        return Math.floor(Math.random()* this.state.interval) + (this.state.interval / 0.5) 
+    }
 
     componentDidMount() {
         setInterval(() => {
             this.setState( {
                 current: this.state.streamGen.next().value            
             })
-        }, 1000)
+        }, this.getInterval())
     }
 
     render() {
       return (
-        <h1>{this.state.current ? this.state.current.src_currency : "test"}</h1>
+        <h1>{this.state.current ? this.state.current.recipient.first_name : ""}</h1>
       );
     }
   }
